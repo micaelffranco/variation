83e068407483298b2b9bf1c5078994541e6c0b82
Micaelffranco <micaelffranco@hotmai.com>
2021-09-29 19:56:56 -0400

    Google OATH

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
+// app.get('/', (req, res) => {
+//   res.send({hi: 'there change twice' });
+// });
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there change twice' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    }, (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
 
-const PORT = process.env.PORT || 5000;// heroku env config port
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+);
+
+const PORT = process.env.PORT || 5000; // heroku env config port or locahost
 app.listen(PORT);
