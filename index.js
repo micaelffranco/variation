813ba007f4c3f6284f9a3272372d7e8a550cc571
Micaelffranco <micaelffranco@hotmai.com>
2021-10-02 15:08:05 -0400

    Adding cookies

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
+// commentario 1
+
 const app = express();
 
-require('./routes/authRoutes')(app);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app); //check this again
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
