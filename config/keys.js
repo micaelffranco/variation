3240c5a73245afbfa14cd40a1f68a1c344556070
Micaelffranco <micaelffranco@hotmai.com>
2021-10-02 16:26:31 -0400

    setting up dev and prod environments

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//keys.js
+if (process.env.NODE_ENV === 'production') {
+  // return prod keys
+  module.exports = require('./dev');
+} else {
+  // return dev keys
+  module.exports = require('./dev');
+}
