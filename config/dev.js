76005c59ec0630a780be67e6acc81da14286dbd6
Micaelffranco <micaelffranco@hotmai.com>
2021-10-03 19:34:20 -0400

    needed

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js dont commit this
+
+module.exports = {
+  googleClientID: '966424058684-l285qb2adgprnpld1eooiu6oo7bvus42.apps.googleusercontent.com',
+  googleClientSecret: '1gUvIPrf1sFmVUghebx4JwcJ',
+  mongoURI: 'mongodb+srv://variation:cfV7UNRv8Kt34UA2@variation.5yqxm.mongodb.net/variation?retryWrites=true&w=majority',
+  cookieKey: 'daljshdlkuashakjsdfhkjsadhfoiuabndiufniasudnfiusadhnfoaaidjnsio'
+}
