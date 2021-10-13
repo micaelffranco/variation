541ad211bdf96ee3b3743a30e548e3a77a081b91
Mick <micaelffranco@hotmail.com>
2021-10-12 20:17:24 -0400

    proxys fixed with redux thunk and axios

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
@@ -7,19 +9,25 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 //container because of materialize css
-function App() {
-  return (
-    <div>
-      <BrowserRouter className="container">
-        <div>
-          <Header />
-          <Route exact={true} path ="/" component={Landing} />
-          <Route exact={true} path ="/surveys" component={Dashboard} />
-          <Route exact={true} path ="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter className="container">
+          <div>
+            <Header />
+            <Route exact={true} path ="/" component={Landing} />
+            <Route exact={true} path ="/surveys" component={Dashboard} />
+            <Route exact={true} path ="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
